bfbceb7672028c278b8e984c71e0696eeb7975d6
gleb.sieemshchikov <gleb.sieemshchikov@jetbrains.com>
2024-02-08 13:11:04 +0100

    Improved email field in the registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,7 +19,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Enter your email address',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    'autofocus' => 'autofocus',
+                    'placeholder' => 'Please enter your email address',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your email address'
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email address'
+                    ])
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
                 'constraints' => [
